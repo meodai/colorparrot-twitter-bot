84e8b1d1e97e60a6b4d88471d4d394563a76627a
gorz.nic@gmail.com <gorz.nic@gmail.com>
2019-03-26 14:44:12 +0200

    changed src/sendImageToUser.js

--- src/sendImageToUser.js
+++ src/sendImageToUser.js
@@ -1,10 +1,4 @@
 const sendImage = require('./lib/sendImage');
-const checkIfColorExistsInTwitts = require(
-  './redis/checkIfColorExistsInTwitts'
-);
-const addColorNameInPostedTwitts = require(
-  './redis/addColorNameInPostedTwitts'
-);
 
 async function sendImageToUser(T, image, status) {
   const b64content = image.toString('base64');
