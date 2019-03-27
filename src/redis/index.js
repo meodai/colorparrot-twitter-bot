0f40a08cbdbc59c6a3e1788368ae0757a9879adf
gorz.nic@gmail.com <gorz.nic@gmail.com>
2019-03-27 23:28:23 +0200

    changed src/redis/index.js

--- src/redis/index.js
+++ src/redis/index.js
@@ -2,9 +2,12 @@ const Redis = require('ioredis');
 
 
 const redis = new Redis(process.env.REDIS_URL, {
-  retryStrategy: (e) => {
-    console.log('cannot connect to redis', e);
-    process.exit();
+  retryStrategy: (times) => {
+    if (times > 3) {
+      console.log('cannot connect to redis');
+      process.exit(1);
+    }
+    return 5000; // ms
   },
 });
 
