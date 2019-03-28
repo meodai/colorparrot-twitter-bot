7047f33e306e89ff0e5058384f3c5200863096bd
gorz.nic@gmail.com <gorz.nic@gmail.com>
2019-03-28 17:23:30 +0200

    dummy commit

--- src/utils/twitter/sendText.js
+++ src/utils/twitter/sendText.js
@@ -1,7 +1,11 @@
 module.exports = (T, params) => {
-  T.post('statuses/update', params, (err) => {
+  T.post('statuses/update', params, (err, data, responce) => {
     if (err) {
+      console.log('broke here')
       console.log(err);
+    } else {
+      console.log(data, 'data')
+      console.log(responce, 'responce')
     }
   });
 };
