bab09c13f68d8abcd3fa824e70cb85f1e2e2e264
gorz.nic@gmail.com <gorz.nic@gmail.com>
2019-03-28 19:45:54 +0200

    changed src/utils/twitter/sendImageToUser.js

--- src/utils/twitter/sendImageToUser.js
+++ src/utils/twitter/sendImageToUser.js
@@ -1,5 +1,11 @@
 const sendImage = require('./sendImage');
 
+/**
+ * @param {object} T, The instance of Twit class
+ * @param {buffer} image, image buffer
+ * @param {string} status, payload with image (name of color...)
+ * @return {undefined}
+ */
 async function sendImageToUser(T, image, status) {
   const b64content = image.toString('base64');
   sendImage(T, b64content, status); // input redis here!
