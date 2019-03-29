3a3d74e7892f740597272107eca9ec5e9407635d
gorz.nic@gmail.com <gorz.nic@gmail.com>
2019-03-29 12:43:35 +0200

    changed src/utils/middlewareClass.js

--- src/utils/middlewareClass.js
+++ src/utils/middlewareClass.js
@@ -10,10 +10,20 @@ class Middleware {
   run() {
     for (let i = 0; i < this.listOfMiddlewares.length; i++) {
       const f = this.listOfMiddlewares[i];
-      this.listOfMiddlewares[i] = async () => {
-        await f(this.T, this.tweet, this.listOfMiddlewares[i + 1])
-            .catch((e) => console.log(e));
-      };
+      if (f.constructor.name === 'Function') {
+        this.listOfMiddlewares[i] = () => {
+          try {
+            f(this.T, this.tweet, this.listOfMiddlewares[i + 1]);
+          } catch (e) {
+            console.log(e);
+          }
+        };
+      } else if (f.constructor.name === 'AsyncFunction') {
+        this.listOfMiddlewares[i] = async () => {
+          await f(this.T, this.tweet, this.listOfMiddlewares[i + 1])
+              .catch((e) => console.log(e));
+        };
+      }
     }
     this.listOfMiddlewares[0]();
   }
