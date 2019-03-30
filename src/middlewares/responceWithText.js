fd7318ea4b2e6a7557d44e7bbfc7fd61b8890329
gorz.nic@gmail.com <gorz.nic@gmail.com>
2019-03-30 20:45:56 +0200

    updated src/middlewares/responceWithText.js

--- src/middlewares/responceWithText.js
+++ src/middlewares/responceWithText.js
@@ -42,7 +42,7 @@ module.exports = async (T, tweet, next, db) => {
 
     await T.statusesUpdate({
       status: `@${screenName} What?! You need to give me a Name and ` +
-        `a color as a hex value... --> ${filteredMessage}. And if you want` +
+        `a color as a hex value... --> ${filteredMessage}. And if you want ` +
         `to know the name of color just ask me: What if the name of #hex`,
     });
 
