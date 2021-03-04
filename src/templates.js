53cec632c7f0200e92de5dce5b59009d7b094359
John Afolayan <jnafolayan@gmail.com>
2021-03-04 08:52:19 +0100

    use json file to store templates

--- src/templates.js
+++ src/templates.js
@@ -1,3 +1,4 @@
+const templatesJSON = require('./templates.json');
 const templates = {};
 const Templates = {
   IMAGE_RESPONSE: 'IMAGE_RESPONSE',
@@ -8,6 +9,11 @@ const Templates = {
   CLOSEST_HEX_NAME_RESPONSE: 'CLOSEST_HEX_NAME_RESPONSE',
 };
 
+/**
+ * Picks a random template from a category for compilation
+ * @param {String} category The template category
+ * @param {Object} opts Template variables
+ */
 const buildMessage = (category, opts) => {
   const tmpls = templates[category];
   const idx = Math.floor(Math.random() * tmpls.length);
@@ -15,6 +21,11 @@ const buildMessage = (category, opts) => {
   return executeTemplate(tmpl, opts);
 };
 
+/**
+ * Compiles a template with the passed variables
+ * @param {String} tmpl The template to execute
+ * @param {Object} opts Variables for the template
+ */
 const executeTemplate = (tmpl, opts) => {
   const entries = Object.entries(opts);
   let result = tmpl;
@@ -25,6 +36,11 @@ const executeTemplate = (tmpl, opts) => {
   return result;
 };
 
+/**
+ * Registers a new template for a category
+ * @param {String} category The template category
+ * @param {String} tmpl The template string
+ */
 const registerTemplate = (category, tmpl) => {
   if (!templates.hasOwnProperty(category)) {
     templates[category] = [];
@@ -32,41 +48,9 @@ const registerTemplate = (category, tmpl) => {
   templates[category].push(tmpl.trim());
 };
 
-registerTemplate(
-  Templates.IMAGE_RESPONSE, 
-  `Hey @{{screenName}}, that's #{{hashTag}}`
-);
-
-registerTemplate(
-  Templates.HEX_TAKEN, 
-  `@{{screenName}} Darn! {{hex}} is taken already. Try shifting the values ` +
-  `a bit and try again`
-);
-
-registerTemplate(
-  Templates.PROPOSAL_ACCEPTED,
-  `@{{screenName}} Thanks for your submission! ` +
-  `Your color-name will be reviewed by a bunch of parrots ` +
-  `and will end up in the color list soon. {{hex}}`
-);
-
-registerTemplate(
-  Templates.PROPOSAL_DENIED,
-  `@{{screenName}} What?! You need to give me a Name and ` +
-  `a color as a hex value... --> {{filteredMessage}}. And if you want ` +
-  `to know the name of color just ask me: What is the name of #hex`
-);
-
-registerTemplate(
-  Templates.EXACT_HEX_NAME_RESPONSE,
-  `@{{screenName}} The name of {{hex}} is {{colorName}}`
-);
-
-registerTemplate(
-  Templates.CLOSEST_HEX_NAME_RESPONSE,
-  `@{{screenName}} We don't have an exact match for {{hex}} ` +
-  `but the closest color we have is {{closestHex}} and its ` +
-  `name is {{closestName}}`
-);
+// register templates in the json file
+Object.entries(templatesJSON).forEach(([category, tmpls]) => {
+  tmpls.forEach((tmpl) => registerTemplate(category, tmpl));
+});
 
 module.exports = { Templates, buildMessage };
