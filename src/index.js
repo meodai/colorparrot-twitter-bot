5a37089efdc9b1bc98068865c5349276de0b1b2f
David Aerne <meodai@gmail.com>
2021-09-12 14:33:26 +0300

    feat(tweets): test user events

--- src/index.js
+++ src/index.js
@@ -155,7 +155,9 @@ async function initialize() {
     await handleIncomingTweet(req, tweetId);
   });
 
-  stream.on("favorite", async (eventMsg) => console.log(eventMsg));
+  const eventStream = T.userEvents('@color_parrot');
+
+  eventStream.on("favorite", async (eventMsg) => console.log(eventMsg));
 
   /**
    * Calculates the difference between now and the next random post time
--- src/utils.js
+++ src/utils.js
@@ -157,6 +157,12 @@ const Twitter = (function() {
         //language: "en",
       });
     }
+
+    userEvents(source) {
+      return this._T.stream("user_event", {
+        source,
+      });
+    }
   }
 
   return { Tweet, Twit };
